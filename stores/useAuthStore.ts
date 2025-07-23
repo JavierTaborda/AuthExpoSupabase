0e2c87aab235ee8669802904be4cd0c6081f5dab
JavierTaborda <javiert.develop@gmail.com>
2025-07-23 11:54:10 -0400

    refactor: useauthstore

--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -1,13 +1,11 @@
 import { supabase } from "@/lib/supabase";
-import { setBiometricEnabled } from "@/utils/biometricFlag";
-import { setSessionStatus } from "@/utils/sessionStatus";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getBiometricEnabled, setBiometricEnabled } from "@/utils/biometricFlag";
+import { getSessionStatus, setSessionStatus } from "@/utils/sessionStatus";
 import { Session } from "@supabase/supabase-js";
 import { router } from "expo-router";
 import { Platform } from "react-native";
 import { create } from "zustand";
 
-// Store interface
 interface AuthStore {
   session: Session | null; // Supabase session
   loading: boolean; // Auth loading status
@@ -17,16 +15,8 @@ interface AuthStore {
   setManualLogin: (value: boolean) => void; // Set manualLogin flag
 
   signIn: (email: string, password: string) => Promise<{ error: Error | null }>; // Sign in with email/password
-  sendCodeOTP: (
-    value: string,
-    method: "email" | "phone",
-    redirectUri?: string
-  ) => Promise<{ error: Error | null }>; // Send OTP
-  signInOTP: (
-    method: string,
-    token: string,
-    type: "email" | "sms"
-  ) => Promise<{ error: Error | null }>; // Verify OTP login
+  sendCodeOTP: (value: string, method: "email" | "phone", redirectUri?: string) => Promise<{ error: Error | null }>; // Send OTP
+  signInOTP: (method: string, token: string, type: "email" | "sms") => Promise<{ error: Error | null }>; // Verify OTP login
   restoreSessionWithBiometrics: () => Promise<{ error: Error | null }>; // Restore session using biometrics
   signUp: (email: string, password: string) => Promise<{ error: Error | null }>; // Register new user
   signOut: () => Promise<void>; // Full sign out
@@ -35,84 +25,85 @@ interface AuthStore {
   initializeAuth: () => Promise<void>; // Initialize session from storage
 }
 
-// Zustand store definition
 export const useAuthStore = create<AuthStore>((set, get) => ({
   session: null,
   loading: true,
   manualLogin: false,
 
-  setSession: (session) => set({ session }), // Save session
-  setManualLogin: (value: boolean) => set({ manualLogin: value }), // Save login method
+  setSession: (session) => set({ session }),
+  setManualLogin: (value) => set({ manualLogin: value }),
 
-  // Sign in with password
   signIn: async (email, password) => {
     set({ loading: true });
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (data.session) {
-      await setSessionStatus("active");
-      await setBiometricEnabled(true);
-      set({ session: data.session, loading: false, manualLogin: true });
-    } else {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+      if (data.session) {
+        await setSessionStatus("active");
+        await setBiometricEnabled(true);
+        set({ session: data.session, manualLogin: true });
+      }
+      return { error };
+    } catch (err) {
+      return { error: err as Error };
+    } finally {
       set({ loading: false });
     }
-
-    return { error };
   },
 
-  // Send OTP to email or phone
-  sendCodeOTP: async (value, method, redirectUri?) => {
-    let response;
-    if (method === "email") {
-      response = await supabase.auth.signInWithOtp({
-        email: value,
-        options: { emailRedirectTo: redirectUri },
-      });
-    } else {
-      response = await supabase.auth.signInWithOtp({ phone: value });
+  sendCodeOTP: async (value, method, redirectUri) => {
+    try {
+      const response =
+        method === "email"
+          ? await supabase.auth.signInWithOtp({
+            email: value,
+            options: { emailRedirectTo: redirectUri },
+          })
+          : await supabase.auth.signInWithOtp({ phone: value });
+
+      return { error: response.error };
+    } catch (err) {
+      return { error: err as Error };
     }
-
-    return { error: response.error };
   },
 
-  // Sign in using OTP code
   signInOTP: async (method, token, type) => {
     set({ loading: true });
+    try {
+      const payload =
+        type === "sms"
+          ? { phone: method, token, type }
+          : { email: method, token, type };
 
-    const payload =
-      type === "sms"
-        ? { phone: method, token, type }
-        : { email: method, token, type };
+      const { data, error } = await supabase.auth.verifyOtp(payload);
 
-    const { data, error } = await supabase.auth.verifyOtp(payload);
+      if (data?.session?.access_token && data?.session?.refresh_token) {
+        await setSessionStatus("active");
+        await setBiometricEnabled(true);
+        set({ session: data.session, manualLogin: true });
+      }
 
-    if (data?.session?.access_token && data?.session?.refresh_token) {
-      await setSessionStatus("active");
-      await setBiometricEnabled(true);
-      set({ session: data.session });
+      return { error };
+    } catch (err) {
+      return { error: err as Error };
+    } finally {
+      set({ loading: false });
     }
-
-    set({ loading: false, manualLogin: true });
-    return { error };
   },
 
-  // Restore user session using biometrics
   restoreSessionWithBiometrics: async () => {
-    const isBiometricEnabled =
-      (await AsyncStorage.getItem("biometricEnabled")) === "true";
-    if (!isBiometricEnabled) {
-      const platformMsg =
-        Platform.OS === "ios"
-          ? "Face ID o Touch ID no están disponibles."
-          : "La autenticación biométrica no está disponible en este dispositivo.";
-
-      return { error: new Error(platformMsg) };
-    }
-
     try {
+      const isBiometricEnabled = await getBiometricEnabled()
+
+      if (!isBiometricEnabled) {
+        const platformMsg =
+          Platform.OS === "ios"
+            ? "Face ID o Touch ID no están disponibles."
+            : "La autenticación biométrica no está disponible en este dispositivo.";
+
+        return { error: new Error(platformMsg) };
+      }
+
+
       const { data, error } = await supabase.auth.getSession();
       if (data?.session) {
         set({ session: data.session, loading: false, manualLogin: true });
@@ -130,54 +121,53 @@ export const useAuthStore = create<AuthStore>((set, get) => ({
     }
   },
 
-  // Register a new user
   signUp: async (email, password) => {
     set({ loading: true });
-    const { error } = await supabase.auth.signUp({ email, password });
-    set({ loading: false });
-    return { error };
+    try {
+      const { error } = await supabase.auth.signUp({ email, password });
+      return { error };
+    } catch (err) {
+      return { error: err as Error };
+    } finally {
+      set({ loading: false });
+    }
   },
 
-  // Log out fully (clears session and biometric flags)
   signOut: async () => {
     await supabase.auth.signOut();
-    await AsyncStorage.setItem("biometricEnabled", "false");
     await setSessionStatus("loggedOut");
     await setBiometricEnabled(false);
     set({ session: null, loading: false });
     router.replace("/(auth)/sign-in");
   },
 
-  // Soft log out (used if biometrics fail, keeps biometric enabled)
   signOutSoft: async () => {
     set({ session: null });
-    await AsyncStorage.setItem("biometricEnabled", "true");
     await setSessionStatus("loggedOut");
     await setBiometricEnabled(true);
     router.replace("/(auth)/sign-in");
   },
 
-  // Load session from storage on app startup
   initializeAuth: async () => {
     set({ loading: true });
 
-    const sessionStatus = await AsyncStorage.getItem("sessionStatus");
- 
-    if (sessionStatus !== "active") {
-      set({ session: null, loading: false });
-      return;
+    const status = await getSessionStatus();
+    if (status !== "active") {
+      set({ session: null });
+      return set({ loading: false });
     }
 
     try {
       const { data, error } = await supabase.auth.getSession();
       if (!data.session || error) {
-        set({ session: null, loading: false });
-        return;
+        set({ session: null });
+      } else {
+        set({ session: data.session });
       }
-      
-      set({ session: data.session, loading: false })
-    } catch (error) {
-      set({ session: null, loading: false });
+    } catch (err) {
+      set({ session: null });
+    } finally {
+      set({ loading: false });
     }
   },
 }));
